4162713e87a42f64a2132da4604a77199070cedb
Simon <simon.quain@gmail.com>
2020-05-23 15:34:13 +0100

    Add hostname to alert output

--- monthemon.sh
+++ monthemon.sh
@@ -10,7 +10,7 @@ SERVICE=$1
 
 case "$(pidof ${SERVICE} | wc -w)" in
 
-  0) curl -d chat_id=${CHAT_ID} -d text="ALERT: ${SERVICE} has stopped running" https://api.telegram.org/bot${API_KEY}/sendMessage
+  0) curl -d chat_id=${CHAT_ID} -d text="ALERT: ${SERVICE} has stopped running on $(hostname)" https://api.telegram.org/bot${API_KEY}/sendMessage
      ;;
   1) 
     ;;
